3eb666e5ed42b27f2197ae9af324dda1652e7207
Tammy <tammycollinse@gmail.com>
2022-02-27 10:37:26 -0500

    commit tree bf4f90832281bcf5b5ebe29704d9cfacbef4e14c

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = tutorials.map(function (tutName){
+const wordArray = tutName.split(" ");
+for (let i = 0; i < wordArray.length; i++){
+ let letter = wordArray[i].charAt(0).toLocaleUpperCase();
+ let realWords = wordArray[i].replace(wordArray[i].charAt(0), letter )
+ wordArray[i] = realWords;
 }
+let titles = (wordArray.join(" "));
+return titles;
+});
+
+
+
+console.log(titleCased);
+
